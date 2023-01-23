bf9daa7aada2e5ab6d28dbeceb2a9fa384bfcf94
unknown <guillaume.faugeron@ynov.com>
2023-01-23 10:44:47 +0100

    lint

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from bottle import route, run
 from sum import sum
 
 
-@route('/')
+@route("/")
 def index():
     return "Hello World !"
 
 
-@route('/add/<a>/<b>')
+@route("/add/<a>/<b>")
 def add(a, b):
-    return {'result': sum(a, b)}
+    return {"result": sum(a, b)}
 
 
-run(host='localhost', port=8080)
+run(host="localhost", port=8080)
